a9197a7e243c770996e978f4e29b288cf058f076
asethia3425 <asethia5664@gmail.com>
2023-10-07 15:41:46 +0530

    done

---
+++ app.js
@@ -0,0 +1,8 @@
+// console.log("gdhj")
+// prompt("dgchjvhkbjxlnj;l");
+console.log(document.getElementById("footerid").innerText);
+
+console.log(document.getElementById("footerid").textContent);
+
+console.log(document.getElementById("footerid").innerHTML);
+
